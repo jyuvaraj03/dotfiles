55f583f0399b690a89292713840cdbce43449750
Yuvaraj J <jyuvaraj03@gmail.com>
2024-09-28 11:55:03 +0530

    Fix yuvim install

--- yuvim/install.sh
+++ yuvim/install.sh
@@ -1,24 +1,30 @@
 sudo_cmd=""
-if [ ! -x "$(command -v sudo)" ]; then
+# if sudo is available, set sudo_cmd
+if [ -x "$(command -v sudo)" ]; then
   sudo_cmd="sudo"
 fi
 # Install dependencies
-$sudo_cmd apt install cmake curl xclip ripgrep
+$sudo_cmd apt-get update -y
+$sudo_cmd apt-get -y install xclip ripgrep
+$sudo_cmd apt-get -y install ninja-build gettext cmake unzip curl build-essential
 
 # Install neovim from source if nvim is not installed
 if [ ! -x "$(command -v nvim)" ]; then
-  VERSION="v0.10.1"
-  curl -sL https://github.com/neovim/neovim/archive/refs/tags/${VERSION}.tar.gz | tar -xzC /tmp 2>&1
-  cd /tmp/neovim-${VERSION}
-  make && make CMAKE_INSTALL_PREFIX=/usr/local/nvim install
-  ln -sf /usr/local/nvim/bin/nvim /usr/local/bin/nvim
-  rm -rf /tmp/neovim-${VERSION}
+  VERSION="0.10.1"
+  mkdir -p downloadtmp
+  curl -sL https://github.com/neovim/neovim/archive/refs/tags/v${VERSION}.tar.gz | tar -xzC downloadtmp 2>&1
+  cd downloadtmp/neovim-${VERSION}
+  $sudo_cmd make && $sudo_cmd make CMAKE_INSTALL_PREFIX=/usr/local/nvim install
+  $sudo_cmd ln -sf /usr/local/nvim/bin/nvim /usr/local/bin/nvim
+  rm -rf downloadtmp
 fi
 
-# Backup existing config
-if [ -d $HOME/.config/nvim ]; then
-  mv $HOME/.config/nvim $HOME/.config/nvim.old
-fi
-
-# Download config from github
+# Download config from github -- downloads at the home directory of whichever user is running the script
 git clone https://github.com/jyuvaraj03/yuvim.git $HOME/.config/nvim
+
+# Loop through all other normal users and install config
+for user in $(ls /home); do
+  if [ -d /home/$user/.config/nvim ]; then
+    mv /home/$user/.config/nvim /home/$user/.config/nvim.old
+  fi
+
